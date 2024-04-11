4cede233e8aabb7237407a403940e371c6fddc8a
Esteban Daniel Gomez Lousa <egomezlousa@frba.utn.edu.ar>
2024-04-10 23:44:56 -0300

    Update Library.hs

--- src/Library.hs
+++ src/Library.hs
@@ -4,73 +4,98 @@ import PdePreludat
 doble :: Number -> Number
 doble numero = numero + numero
 
+
+----------- PARTE 1 -----------
+
+-- Nomu modelado con tuplas
+-- type Nomu = (Bool,      Bool,                 Number,       String,      Number, Number) 
+        --     (TieneAlas, TieneMultiplesBrazos, CantidadOjos, ColorDePiel, Vida,   Fuerza)
+
+
+-- Usando typenames
 type TieneAlas = Bool
 type TieneMultiplesBrazos = Bool
 type CantidadOjos = Number
 type Vida = Number
 type Fuerza = Number
-data ColorDePiel = GRIS | AZUL | BLANCO | NEGRO deriving(Show)
 
-data Categoria = HIGH_END | FUERTE | COMUN | PICHI deriving(Show) --estos deriving para que se vea en consola si haces pepe de consulta
+type Nomu = (TieneAlas, TieneMultiplesBrazos, CantidadOjos, ColorDePiel, Vida, Fuerza)
 
----Parte 1
 
-type Nomu = (TieneAlas, TieneMultiplesBrazos, CantidadOjos, ColorDePiel, Vida, Fuerza) 
+-- Usando data y enums para ColorDePiel
+data ColorDePiel = GRIS | AZUL | BLANCO | NEGRO deriving (Show)
 
-pepe::Nomu
+
+-- Instanciamos algunos Nomus con tuplas
+pepe :: Nomu
 pepe = (True, False, 0, GRIS, 200, 100)
 
-juan::Nomu
+juan :: Nomu
 juan = (True, True, 4, NEGRO, 1000, 3500)
+
+
+-- Averiguamos si un Nomu tiene ojos
+tieneOjos :: Nomu -> Bool
+tieneOjos (_,_,cantidadOjos,_,_,_) = cantidadOjos > 0
+
+
+-- Modelamos las categorías
+data Categoria = HIGH_END | FUERTE | COMUN | PICHI deriving (Show)
 
 categoriaConTuplas :: Nomu -> Categoria
 categoriaConTuplas (_,_,_,_,_,fuerza)
     | fuerza > 10000 = HIGH_END
     | fuerza > 3000 = FUERTE
     | fuerza > 1000 = COMUN
-    |otherwise = PICHI
-
-noTieneOjos :: Nomu -> Bool
-noTieneOjos (_,_,cantidadOjos,_,_,_) = cantidadOjos == 0
-
----Parte 2
+    | otherwise = PICHI
 
 
-data DataNomu = UnNomu{
+----------- PARTE 2 -----------
+
+-- Ahora modelamos todo con data
+data DataNomu = UnNomu {
     tieneAlas :: TieneAlas,
     tieneMultiplesBrazos :: TieneMultiplesBrazos,
     cantidadOjos :: CantidadOjos,
     colorDePiel :: ColorDePiel,
     vida :: Vida,
     fuerza :: Fuerza
-} deriving(Show)
+} deriving (Show)
 
-rocco::DataNomu
+rocco :: DataNomu
 rocco = UnNomu True True 3 GRIS 200 1000
 
-arturo::DataNomu
-arturo = UnNomu{tieneAlas = True, tieneMultiplesBrazos = False, cantidadOjos = 0, colorDePiel = BLANCO, vida = 900, fuerza = 5000}
+arturo :: DataNomu
+arturo = UnNomu {
+    tieneAlas = True, 
+    tieneMultiplesBrazos = False, 
+    cantidadOjos = 0, 
+    colorDePiel = BLANCO, 
+    vida = 900, 
+    fuerza = 5000
+}
 
 categoriaConData :: DataNomu -> Categoria
 categoriaConData nomu
     | fuerza nomu > 10000 = HIGH_END
     | fuerza nomu > 3000 = FUERTE
     | fuerza nomu > 1000 = COMUN
-    |otherwise = PICHI
+    | otherwise = PICHI
 
-noTieneOjosConData :: DataNomu -> Bool
-noTieneOjosConData nomu = cantidadOjos nomu == 0 
+tieneOjosConData :: DataNomu -> Bool
+tieneOjosConData nomu = cantidadOjos nomu > 0 
 
---- Parte 3
 
-data Poder = UnPoder{
+----------- PARTE 3 -----------
+-- Ahora modelamos los poderes
+data Poder = UnPoder {
     cantidadDeCuracionPorUso :: Number,
     cantidadDeDañoPorUso :: Number,
     rangoDeAtaque :: Number,
-    probabilidadDeDañoCritico :: Number
+    probabilidadDeDanioCritico :: Number
 } deriving (Show)
 
-
+-- Creamos algunos poderes
 superRegeneracion :: Poder
 superRegeneracion = UnPoder 1000 0 5 0 
 
@@ -80,9 +105,11 @@ superFuerza = UnPoder 100 3000 15 0.5
 proyectilesDeAire :: Poder
 proyectilesDeAire = UnPoder 0 2500 500 0.15
 
+
+-- Modelamos y creamos un Nomu con poderes
 type Poderes = [Poder]
 
-{- data DataNomuConPoderes = UnNomuConPoderes{
+data NomuConPoderes = UnNomuConPoderes{
     tieneAlas :: TieneAlas,
     tieneMultiplesBrazos :: TieneMultiplesBrazos,
     cantidadOjos :: CantidadOjos,
@@ -92,14 +119,18 @@ type Poderes = [Poder]
     poderes :: Poderes
 } deriving(Show)
 
-mateo::DataNomuConPoderes
+mateo :: NomuConPoderes
 mateo = UnNomuConPoderes True True 3 GRIS 200 1000 [superRegeneracion, superFuerza, proyectilesDeAire]
 
-probabilidadDeCritico :: DataNomuConPoderes-> Number
-probabilidadDeCritico nomu = probabilidadDeDañoCritico (last(poderes nomu))   -} -- hay que comentar el DataNomu de mas arriba antes de descomentar este para que no tire error de definiciones!!
 
+-- Averiguamos la probabilidad de daño crítico del último poder conseguido por un Nomu
+probabilidadDeCritico :: NomuConPoderes -> Number
+probabilidadDeCritico nomu = probabilidadDeDanioCritico (last (poderes nomu))
+
+-- Averiguamos si un poder es cuerpo a cuerpo
 esUsadoCuerpoACuerpo :: Poder -> Bool
 esUsadoCuerpoACuerpo poder = rangoDeAtaque poder < 100
 
+-- Averiguamos si un poder es solamente de curación
 esSolamenteDeCuracion :: Poder -> Bool
-esSolamenteDeCuracion poder = (cantidadDeCuracionPorUso poder > 0) && (cantidadDeDañoPorUso poder <= 0)
+esSolamenteDeCuracion poder = (cantidadDeCuracionPorUso poder > 0) && (cantidadDeDañoPorUso poder == 0)
